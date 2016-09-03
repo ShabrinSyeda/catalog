c0cf02fa9cc454a6784f7bb09db6afe20126360b
divyar <divyar@lotvue.com>
2016-09-03 12:45:58 +0530

    catalog demo to list users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = 'ab34461936e2ed7eb430db8c91e45cd940aacc47b9be028ec8ef7ccc87f57ca8d783e06f2b2b9e135bfc215e2915bf2d6645f0c2b95791cf9cf4ee1bd49d8a1b'
